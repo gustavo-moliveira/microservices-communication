dcc43098400ca46c5024d6696debc0a0bb5892c7
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-09-06 12:50:07 -0300

    add token jwt implemation

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,17 +1,26 @@
 import express from "express";
+import * as db from "./src/config/db/initialData.js";
+import userRoutes from "./src/modules/user/routes/UserRoutes.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
+db.createInitialData();
+
+
 app.get('/api/status', (req, res) => {
-    return res.json({
+    return res.status(200).json({
         service: "Auth-API",
         status: "up",
         httpStatus: 200
-    })
-})
+    });
+});
+
+app.use(express.json());
+app.use(userRoutes);
+
 
 app.listen(PORT, () =>{
     console.info(`Server started sucessfully at port ${PORT}`);
-})
+});
---
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import checkToken from "../../../config/auth/checkToken.js";
+
+import UserController from "../controller/UserController.js";
+
+const router = new Router();
+
+router.post('/api/user/auth', UserController.getAccessToken);
+
+router.use(checkToken);
+
+router.get('/api/user/email/:email', UserController.findByEmail);
+
+export default router;
